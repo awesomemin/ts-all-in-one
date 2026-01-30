38491a4946d8d268ff811d12baaa6f67363bbad2
Seongmin Oh <saogig0503@naver.com>
2026-01-30 21:46:58 +0900

    Partial

---
+++ src/15_partial.ts
@@ -0,0 +1,20 @@
+interface Profile {
+  name: string;
+  age: number;
+  married: boolean;
+}
+
+const seongmin: Profile = {
+  name: 'seongmin',
+  age: 25,
+  married: true,
+};
+
+const seongminAnonymouse: Partial<Profile> = {
+  age: 25,
+  married: true,
+};
+
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
+};
