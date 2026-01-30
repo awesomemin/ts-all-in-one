12af48dec7cbbc823a672f9c4736254b16be28f5
Seongmin Oh <saogig0503@naver.com>
2026-01-30 21:33:57 +0900

    오버로딩

---
+++ src/14_overloading.ts
@@ -0,0 +1,7 @@
+declare function add(x: number, y: number): number;
+declare function add(x: string, y: string): string;
+declare function add(x: number, y: number, z: number): number;
+
+add(1, 2);
+add(1, 5, 14);
+add('1', '25');
